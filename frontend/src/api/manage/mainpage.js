1e7584b59809204555a73f14b0a44f0d84119b27
Fanchengcheng2020201680 <12135838+fanchengcheng2020201680@user.noreply.gitee.com>
2022-12-17 08:40:08 +0000

    update frontend/src/api/manage/mainpage.js.
    Signed-off-by: Fanchengcheng2020201680 <12135838+fanchengcheng2020201680@user.noreply.gitee.com>

--- frontend/src/api/manage/mainpage.js
+++ frontend/src/api/manage/mainpage.js
@@ -1,21 +1,67 @@
-import {getMockHotTags,getMockNewTags,getMockHotPosts,getMockNewPosts} from '@/mock/mainpagedata'
+import request from '@/utils/request'
+import { useMock } from '../settings'
+import {HotTagsData,NewTagsData,HotPostsData,NewPostsData} from '@/mock/mainpagedata'
 
 export function getHotTags(payload){
     const data = payload;
-    return getMockHotTags(payload)
+    return  useMock ? new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(HotTagsData)
+        },100)
+      }) : request({
+        url: '/gettag',
+        headers: {
+          isToken: false
+        },
+        method: 'post',
+        data: data.HotTagsNum
+      })
 }
 
 export function getNewTags(payload){
     const data = payload;
-    return getMockNewTags(payload)
+    return  useMock ? new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(NewTagsData)
+        },100)
+      }) : request({
+        url: '/gettag',
+        headers: {
+          isToken: false
+        },
+        method: 'post',
+        data: data.NewTagsNum
+      })
 }
 
 export function getHotPosts(payload){
     const data = payload;
-    return getMockHotPosts(payload)
+    return  useMock ? new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(HotPostsData)
+        },100)
+      }) : request({
+        url: '/gettag',
+        headers: {
+          isToken: false
+        },
+        method: 'post',
+        data: data.HotPostsNum
+      })
 }
 
 export function getNewPosts(payload){
     const data = payload;
-    return getMockNewPosts(payload)
+    return  useMock ? new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(NewPostsData)
+        },100)
+      }) : request({
+        url: '/gettag',
+        headers: {
+          isToken: false
+        },
+        method: 'post',
+        data: data.NewPostsNum
+      })
 }
