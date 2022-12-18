52e11c4cea9f8843ddb8901c5ff90cf9e4766c76
Fanchengcheng2020201680 <12135838+fanchengcheng2020201680@user.noreply.gitee.com>
2022-12-18 14:20:52 +0000

    add frontend/src/api/manage/visualization.js.
    Signed-off-by: Fanchengcheng2020201680 <12135838+fanchengcheng2020201680@user.noreply.gitee.com>

---
+++ frontend/src/api/manage/visualization.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+import { useMock } from '../settings'
+import {HotTagsData,NewTagsData,HotPostsData,NewPostsData} from '@/mock/mainpagedata'
+
+export function getData(startdate,enddate){
+    const data = {startDate:startdate,endDate:enddate};
+    return  useMock ? new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(HotTagsData)
+        },100)
+      }) : request({
+        url: '/getHotTags',
+        headers: {
+          isToken: false
+        },
+        method: 'post',
+        data: data
+      })
+}
