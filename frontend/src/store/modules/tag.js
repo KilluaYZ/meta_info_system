b43797ff9ac04ed084299f5c36deebd8ea22fb1d
黄海彬 <12203267+icemexic@user.noreply.gitee.com>
2022-12-21 02:10:54 +0000

    update frontend/src/store/modules/tag.js.
    Signed-off-by: 黄海彬 <12203267+icemexic@user.noreply.gitee.com>

--- frontend/src/store/modules/tag.js
+++ frontend/src/store/modules/tag.js
@@ -2,8 +2,15 @@ const state = {
     presetParam:{},
     needRunPreset:false
 }
+
 const mutations = {
-    
+    setpresetParam: (state,preload) => {
+        state.presetParam = preload
+        if(state.presetParam != undefined)
+            state.needRunPreset = true;
+        console.log('传递初始化数据')
+        console.log(state.presetParam)
+    }
 }
 
 const actions = {
